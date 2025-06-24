38c2a8a6fb7dbd75acbdaeb7068b5ea4f5b22619
nhanpham4013 <nhanpham4013@gmail.com>
2025-06-24 21:30:58 +0700

    Add default renderer classes to REST framework settings for improved API UI

--- ecommerce_db/ecommerce_db/settings.py
+++ ecommerce_db/ecommerce_db/settings.py
@@ -99,12 +99,16 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-        'rest_framework.renderers.BrowsableAPIRenderer'
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        
   
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',  # <== THIS enables the UI
+    ]
 }
 
 
